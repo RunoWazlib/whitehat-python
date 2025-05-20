44315c3bdf662f3673cd52da0a8a724ff8975df1
RunoWazlib <ethanrog01@gmail.com>
2025-05-20 00:29:46 -0500

    Base scanner functionality + pipfile

---
+++ port-scanner/main.py
@@ -0,0 +1,31 @@
+import socket
+import datetime
+
+def scan_ports(target, port_range=range(1,1025)):
+    print(f"\n[*] Scanning target: {target}")
+    print(f"\n[*] Start time: {datetime.now()}")
+
+    for port in port_range:
+        # Define a socket stream for IPv4 adresses and TCP connection
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Try to connect for 0.5 s, then move on
+        sock.settimeout(0.5)
+
+        result = sock.connect_ex((target, port))
+        
+        # Check if port is open
+        if result == 0:
+            try:
+                # Check what the service looks like (ssh, http, https, etc.)
+                service = socket.getservbyport(port)
+            except:
+                # Service is unusual or not recognized
+                service = "Unknown"
+            print(f"[+] Port {port} is open - ({service})")
+            sock.close()
+        else:
+            print(f"[-] Port {port} is closed")
+
+if __name__ == "__main__":
+    target_host = input("Enter IP or Hostname to scan:\n")
+    scan_ports(target_host)
